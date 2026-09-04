f0131aa821ab055e0b30ed486ea9375af60810eb
luananasseh <luananasseh@gmail.com>
2026-09-04 18:09:53 -0300

    Add files via upload

--- SPRINT 2/GABRIELA/main.py
+++ SPRINT 2/GABRIELA/main.py
@@ -1,6 +1,5 @@
-def calcula_soma(x, y):
-    return x + y
+def calcula_multiplicacao(x,y):
+    return x * y
 
-
-def calcula_subtracao(x, y):
-    return x - y
+def calcula_divisao(x,y):
+    return x/y
